d53478d756ef4ca6e4232c6c0714e8698cb07988
abdelghani <aaittamghart9@gmail.com>
2024-09-19 11:35:20 +0100

    with comments

--- src/service/ForestService.java
+++ src/service/ForestService.java
@@ -1,4 +1,33 @@
 package service;
 
+import model.Forest;
+import repository.ForestRepository;
+import repository.TreeRepository;
+
+import java.util.List;
+
 public class ForestService {
+
+    private ForestRepository forestRepository;
+    private TreeRepository treeRepository;
+
+    public ForestService() {
+        this.forestRepository = new ForestRepository();
+        this.treeRepository = new TreeRepository();
+    }
+    public Boolean saveAll(List<Forest> forests) {
+        for (Forest forest : forests) {
+            if (forestRepository.save(forest).isEmpty())
+                return false;
+        }
+        return true;
+    }
+
+    public List<Forest> findAll() {
+        List<Forest> forests = forestRepository.findAll();
+        forests.forEach(
+                forest -> forest.setTrees(treeRepository.findByForestId(forest))
+        );
+        return forests;
+    }
 }
--- src/service/TreeService.java
+++ src/service/TreeService.java
@@ -1,4 +1,25 @@
 package service;
 
+import model.Tree;
+import repository.TreeRepository;
+
+import java.util.List;
+
 public class TreeService {
+    private TreeRepository treeRepository;
+
+    public TreeService() {
+        this.treeRepository = new TreeRepository();
+    }
+    public Boolean save(Tree tree) {
+        return treeRepository.save(tree).isPresent();
+    }
+
+    public Boolean saveAll(List<Tree> trees) {
+        for (Tree tree : trees) {
+            if (treeRepository.save(tree).isEmpty())
+                return false;
+        }
+        return true;
+    }
 }
